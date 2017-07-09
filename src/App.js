df77c12fb9c194a1b2354acf9f2e8816804c653e
pmurias <pawelmurias@gmail.com>
2017-07-09 16:48:57 +0200

    Start of very simple routing

--- src/App.js
+++ src/App.js
@@ -3,18 +3,32 @@ import './App.css';
 
 import Type from './Type';
 
+import {BrowserRouter as Router, Route, Link} from 'react-router-dom'
+
 const frame = require('./Frame.json');
 const baggy = require('./Baggy.json');
-const types = [baggy];
+const types = {'Backtrace::Frame': frame, 'Baggy': baggy};
+
+class DocsForType extends Component {
+  render() {
+    const type = types[this.props.match.params.name];
+    return <Type methods={type.methods} name={type.name} desc={type.desc} kind={type.kind}/>
+  }
+}
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        {types.map((c,idx) => <Type key={idx} methods={c.methods} name={c.name} desc={c.desc} kind={c.kind}/>)}
-      </div>
+      <Router>
+          <div>
+            <div><Link to="/type/Baggy">Baggy</Link></div>
+            <div><Link to="/type/Backtrace::Frame">Backtrace::Frame</Link></div>
+            <Route path="/type/:name" component={DocsForType}></Route>
+          </div>
+      </Router>
     );
   }
 }
+
 
 export default App;
