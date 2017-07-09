53bdc026e0fc111abbec35f426a5905d6cad393a
pmurias <pawelmurias@gmail.com>
2017-07-09 20:17:54 +0200

    Don't have a Method sections when we have no methods

--- src/Type.js
+++ src/Type.js
@@ -10,7 +10,7 @@ class Type extends Component {
       <div>
         <h1>{this.props.kind[0].toUpperCase() + this.props.kind.substr(1)} {this.props.name}</h1>
         <Pod contents={this.props.desc}/>
-        <h2>Methods</h2>
+        {this.props.methods.length && <h2>Methods</h2>}
         {this.props.methods.map((method, idx) => <Method key={idx} name={method.name} desc={method.desc} sig={method.sig}/>)}
       </div>
     )
